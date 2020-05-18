3b819bdc55a2f09b0742755988e6b44ac468a1bf
Kei Sugano <kei.sugano@h-t.co.jp>
2020-05-18 09:28:46 +0900

    readme and installer added

---
+++ README.md
@@ -0,0 +1,16 @@
+# H2 Client (Selenum based)
+
+## Install
+https://chromedriver.storage.googleapis.com/index.html
+download chromedriver matches the Chrome Browser version
+you are currently using.
+
+```
+$ ./installer
+```
+
+## Usage
+
+```
+$ h2c [query]
+```
--- h2c.py
+++ h2c.py
@@ -8,6 +8,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 import time
 
 def finish():
@@ -17,14 +18,27 @@ def finish():
     else:
       finish()
 
+args = sys.argv[1:]
+if (len(args) == 0):
+    print('\nyou need argument.\n')
+    sys.exit()
+
+query = '; '.join(args)
+
+print("query: {}".format(query))
 driver = webdriver.Chrome()
 driver.get('http://localhost:8080/dbconsole/login.jsp')
 driver.find_element_by_xpath("//input[@type='submit']").click()
-driver.switch_to_frame("h2query")
+driver.switch_to.frame("h2query")
 time.sleep(1)
 elem = driver.find_element_by_xpath("//textarea[@id='sql']")
 elem.clear()
 elem.send_keys("select * from fixed_data")
 elem = driver.find_element_by_xpath("//input[@value='Run']").click()
+#
+# TODO: later...
+#
+# driver.switch_to.frame("h2result")
+# elem = driver.find_elements_by_class("sortHeader")
 time.sleep(3)
 finish()
